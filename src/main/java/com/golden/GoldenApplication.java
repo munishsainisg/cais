563ea815403d9a9439f5a790779bbbcc25d86816
munishsainisg <munishsainisg@gmail.com>
2022-10-19 09:51:03 -0400

    initial
    initial

---
+++ src/main/java/com/golden/GoldenApplication.java
@@ -0,0 +1,13 @@
+package com.golden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/golden/GoldenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
