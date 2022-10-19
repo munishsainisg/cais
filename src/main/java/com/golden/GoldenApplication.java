8286fbe9ebb42b52fb312f8dfdcf2791a1d1797a
munishsainisg <37417576+munishsainisg@users.noreply.github.com>
2022-10-19 10:25:04 -0400

    Update GoldenApplication.java

--- src/main/java/com/golden/GoldenApplication.java
+++ src/main/java/com/golden/GoldenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoldenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GoldenApplication.class, args);
+        SpringApplication.run( GoldenApplication.class, args);
     }
 
 }
